a591f7527cc91473ef5a346ed54ba09f5a4a3ca8
mangcaugai1st <mangcaugai1st@gmail.com>
2023-06-25 21:49:26 +0700

    first commit

---
+++ 2sum/Program(brute-force).cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace CSharp
+{ 
+    class Solution
+    {
+        public static int[] TwoSum(int[] nums, int target) 
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] {i, j};       
+                    }
+                }
+            }
+            return new int[0];
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = new int[] {2, 7, 11, 15};
+            int target = 9;
+            Console.Write(String.Join(" ", Solution.TwoSum(nums, target))); 
+        }
+    }
+}
