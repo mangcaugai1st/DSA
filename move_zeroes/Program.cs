f8bc45ed61bba5f49a41a40679ff3450e885d7c7
mangcaugai 1st <mangcaugai1st@gmail.com>
2024-02-15 20:53:57 +0700

    15-Feb-2024

---
+++ move_zeroes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace CSharp
+{
+    class Program
+    {
+        public static void MoveZeroes(int[] nums)
+        {
+            int left = 0;
+            for (int curIndex = 0; curIndex < nums.Length; curIndex++)
+            {
+                if (nums[curIndex] != 0)
+                {
+                    (nums[left], nums[curIndex]) = (nums[curIndex], nums[left]);
+                    left++;
+                }
+            }
+        }
+        public static void PrintArray(int[] arr)
+        {
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] nums = new int[] { 0, 1, 0, 3, 12 };
+            MoveZeroes(nums);
+            PrintArray(nums);
+        }
+    }
+}
+
+
+
