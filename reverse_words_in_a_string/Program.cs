eebdb260abde1c55c83aa01cfd95f80e4e751e6c
mangcaugai 1st <mangcaugai1st@gmail.com>
2024-02-15 21:25:17 +0700

    15-Feb-2024

---
+++ reverse_words_in_a_string/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace CSharp
+{
+    public class Solution 
+    {
+        public static string ReverseWords(string s)
+        {
+            return string.Join(" ", s.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Reverse());
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string s = " the sky is blue ";
+            Console.Write(Solution.ReverseWords(s));
+        }
+    }
+}
+
+
+
