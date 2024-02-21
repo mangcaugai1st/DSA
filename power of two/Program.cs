04cf77d286ae9327684ab3bc2f16c603b2da0302
mangcaugai 1st <mangcaugai1st@gmail.com>
2024-02-21 09:41:23 +0700

    21-Feb-2024

---
+++ power of two/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace CSharp
+{
+    public class Solution
+    {
+        public static bool IsPowerOfTwo(int n)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                double answer = Math.Pow(2, i);
+                if (answer == n) return true;
+            }
+            return false;
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n1 = 1;
+            Console.WriteLine(Solution.IsPowerOfTwo(n1));
+            int n2 = 16;
+            Console.WriteLine(Solution.IsPowerOfTwo(n2));
+            int n3 = 3;
+            Console.WriteLine(Solution.IsPowerOfTwo(n3));
+        }
+    }
+}
+
+
+
