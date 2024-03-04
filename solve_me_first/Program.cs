7379b25092b4191233592eb1fdf304754b952824
mangcaugai 1st <mangcaugai1st@gmail.com>
2024-03-04 14:31:02 +0700

    04-Mar-2024

---
+++ solve_me_first/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+
+namespace CSharp
+{
+    class Program
+    {
+        static int solveMeFirst(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+        public static void Main(string[] args)
+        {
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write(solveMeFirst(val1, val2));
+        }
+    }
+}
+
+
+
