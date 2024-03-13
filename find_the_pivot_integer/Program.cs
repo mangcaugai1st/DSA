ebf20aee8af6cd72bc44159bdd764fb642f1ee5a
mangcaugai 1st <mangcaugai1st@gmail.com>
2024-03-13 14:57:43 +0700

    13-Mar-2024

---
+++ find_the_pivot_integer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+namespace CSharp
+{
+    public class Solution 
+    {
+        public static int PivotInteger(int n)
+        {
+            int sum = 0;            
+            for (int i = 1; i <= n; i++)
+            {
+               sum += i;
+            }
+            int sumLeft = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sumLeft += i;
+                if (sumLeft < sum)
+                {
+                    sumLeft += i;
+                }
+                else if (sumLeft == sum)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+    class Program
+    {   
+        public static void Main(string[] args)
+        {
+            int n = 8;
+            Console.Write(Solution.PivotInteger(n));
+        }
+    }
+}
+
+
+
