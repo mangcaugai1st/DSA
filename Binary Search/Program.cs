5449f09c3c532f409d8d80d41d9a1c338c594ddf
mangcaugai 1st <mangcaugai1st@gmail.com>
2024-05-15 17:07:22 +0700

    15-May-2024

---
+++ Binary Search/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CSharp
+{
+    public class Solution 
+    {
+        public int Search(int[] nums, int target)
+        {
+            int low = 0;
+            int high = nums.Length - 1;
+            while (low <= high)
+            {
+                int middle = (low + high) / 2;
+                if (target == nums[middle]) return middle;
+                else if (target > nums[middle]) low = middle + 1;
+                else high = middle - 1;
+            }
+            return -1;
+        }
+    }
+    class Program 
+    {    
+        public static void Main(string[] args)
+        {
+            int[] nums = new int[] {-1, 0, 3, 5, 9, 12};
+            int target = 9;
+            Solution testcase1 = new Solution();
+            int result =  testcase1.Search(nums, target);
+            Console.WriteLine(result);
+        }
+    }
+}
+
+
+
+
+
+
+
+
