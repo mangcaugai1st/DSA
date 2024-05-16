e6fa12e2993b1e51582ffcbc1e42113941d5ff95
mangcaugai 1st <mangcaugai1st@gmail.com>
2024-05-16 14:39:51 +0700

    16-May-2024

---
+++ Binary Tree Preorder Traversal  (LeetCode)/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CSharp
+{
+
+    public class TreeNode 
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode (int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public class Solution 
+    {
+        List<int> result = new List<int>();
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            if (root == null) return result;  
+            else 
+            {
+                result.Add(root.val);   
+                PreorderTraversal(root.left);
+                PreorderTraversal(root.right);
+            }
+            return result;
+        }
+    }
+    class Program 
+    {    
+        public static void Main(string[] args)
+        {
+            TreeNode root = new  TreeNode(1); 
+            root.right = new TreeNode(2);   
+            root.right.left = new TreeNode(3);
+            Solution testcase1 = new Solution();    
+            var items = testcase1.PreorderTraversal(root);    
+            foreach (var item in items)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
