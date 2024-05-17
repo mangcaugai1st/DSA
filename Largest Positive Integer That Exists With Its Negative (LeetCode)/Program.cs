a026ca8b80f3c3f9a98b49e6ce104a9a8cf1d055
mangcaugai 1st <mangcaugai1st@gmail.com>
2024-05-17 17:54:39 +0700

    17-May-2024

---
+++ Largest Positive Integer That Exists With Its Negative (LeetCode)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System; 
+
+namespace CSharp
+{
+    public class Solution 
+    {
+        public int FindMaxK(int[] nums)
+        {
+            Array.Sort(nums);
+            int i = 0;
+            int j = nums.Length - 1;
+            while (i < j)
+            {
+                if (nums[i] + nums[j] == 0)
+                {                        
+                    return nums[j];
+                }
+                else if (Math.Abs(nums[i]) > Math.Abs(nums[j])) i++;
+                else j--;   
+            } 
+            return -1; 
+        }
+    }
+    class Program 
+    {    
+        public static void Main(string[] args)
+        {
+            int[] nums = new int[]{-1,10,6,7,-7,1};
+            Solution testcase1 = new Solution();
+            Console.WriteLine(testcase1.FindMaxK(nums));    
+        }
+    }
+}
+
+
+
+
+
+
+
+
