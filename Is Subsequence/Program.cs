05d8e0d65e183dc31c52f3e877a8d63d23f7bc34
mangcaugai 1st <mangcaugai1st@gmail.com>
2024-09-22 12:14:22 +0700

    22-Sep-2024

---
+++ Is Subsequence/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design.Serialization;
+using System.Runtime.Versioning;
+
+namespace CSharp
+{
+    public class Solution 
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            int i = 0;
+            int j = 0;
+            while (i < s.Length && j < t.Length)
+            {
+                if(s[i] == t[j])
+                {
+                    i++;
+                }  
+                j++;
+            } 
+            return i == s.Length ? true : false;
+        }
+    }
+    class Program 
+    {    
+        public static void Main(string[] args)
+        { 
+            Solution testcase1 = new Solution(); 
+            string s = "axc";
+            string t = "ahbgdc";
+            Console.WriteLine(testcase1.IsSubsequence(s, t));
+            
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
