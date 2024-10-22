4d00243dc9569f91a790a5a6201879a4895af818
mangcaugai1st <mangcaugai1st@gmail.com>
2024-10-22 20:20:40 +0700

    22-Oct-2024

---
+++ Missing Number (LeetCode)/Program(HashMap).cs
@@ -0,0 +1,33 @@
+﻿namespace dotnet 
+{ 
+  public class Solution 
+  {
+    public int MissingNumber(int[] nums)
+    {
+      Array.Sort(nums);
+      Dictionary<int, int> map = new Dictionary<int, int>(); 
+      for (int i = 0; i < nums.Length; i++)
+      {
+        map.Add(i, nums[i]);
+      }
+      foreach (var pair in map)
+      {
+        if (pair.Value > pair.Key)
+        {
+          return pair.Value - 1;
+        }
+      }
+      return nums.Length;
+    }
+  }
+  class Program 
+  { 
+    static void Main(string[] args)
+    {      
+      int[] nums = {9,6,4,2,3,5,7,0,1};
+      Solution testcase1 = new Solution(); 
+      Console.WriteLine(testcase1.MissingNumber(nums));
+    }
+  } 
+}
+
